f657a99d9073880e63c00dc4b28f907ff9d4d43c
Jyguy <jskekkx@gmail.com>
2024-08-17 15:25:05 -0400

    refactor: license to GPL3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2024 Jyguy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 plugins {
     `java-library`
     id("xyz.jpenilla.resource-factory-paper-convention") version "1.1.2" // Generates plugin.yml based on the Gradle config
@@ -22,6 +39,9 @@ dependencies {
 }
 
 tasks {
+    jar {
+        from("LICENSE")
+    }
     compileJava {
         // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
         // See https://openjdk.java.net/jeps/247 for more information.
